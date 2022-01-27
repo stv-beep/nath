342cf35b51587597b46a259b5b3ed514c1c2c7f3
stv-beep <aleixjoaquimalguero@iesmontsia.org>
2022-01-27 12:58:51 +0100

    formulari

--- resources/views/activitat-form.blade.php
+++ resources/views/activitat-form.blade.php
@@ -1,85 +1,66 @@
-<!DOCTYPE html>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Activitat del treballador</title>
-
-
+@extends('layouts.app')
 <!--altres-->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" integrity="sha512-aOG0c6nPNzGk+5zjwyJaoRUgCdOrfSDhmMID2u4+OIslr0GjpLKo7Xm0Ao3xmpM4T8AmIouRkqwj1nrdVsLKEQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" integrity="sha512-uto9mlQzrs59VwILcLiRYeLKPPbS/bT71da/OEBYEwcdNUk8jYIy+D176RYoop1Da+f9mvkYrmj5MCLZWEtQuA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" integrity="sha512-aOG0c6nPNzGk+5zjwyJaoRUgCdOrfSDhmMID2u4+OIslr0GjpLKo7Xm0Ao3xmpM4T8AmIouRkqwj1nrdVsLKEQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" integrity="sha512-uto9mlQzrs59VwILcLiRYeLKPPbS/bT71da/OEBYEwcdNUk8jYIy+D176RYoop1Da+f9mvkYrmj5MCLZWEtQuA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!--SCRIPT ALEIX-->   
+<script>
+    //https://stackoverflow.com/questions/41632942/how-to-measure-time-elapsed-on-javascript
+    //https://stackoverflow.com/questions/1210701/compute-elapsed-time/1210726
+    //https://ralzohairi.medium.com/displaying-dynamic-elapsed-time-in-javascript-260fa0e95049
+    
+    var startTime, endTime, h;
 
-    <!--SCRIPT ALEIX-->   
-    <script>
-        /*
-        
-        
-        https://stackoverflow.com/questions/41632942/how-to-measure-time-elapsed-on-javascript
-        https://stackoverflow.com/questions/1210701/compute-elapsed-time/1210726
-        https://ralzohairi.medium.com/displaying-dynamic-elapsed-time-in-javascript-260fa0e95049
-        https://es.stackoverflow.com/questions/30957/c%C3%B3mo-registrar-la-fecha-actual-del-sistema-en-laravel-5 
-        
+    function start() {
+    startTime = new Date();
+    return startTime;
+    };
 
-        */
-        
-        var startTime, endTime, h;
-
-        function start() {
-        startTime = new Date();
-        return startTime;
-        };
-
-        function end() {
-        endTime = new Date();
-        var timeDiff = endTime - startTime; //in ms
-        // strip the ms
-        timeDiff = timeDiff / 1000;
+    function end() {
+    endTime = new Date();
+    var timeDiff = endTime - startTime; //in ms
+    // strip the ms
+    timeDiff = timeDiff / 1000;
 
 
-        // get seconds 
-        var segons = Math.round(timeDiff);
-        var minuts = segons/60;
-        var min = Math.round((minuts + Number.EPSILON) * 100) / 100;
-        var hores = minuts/60;
-        h = Math.round((hores + Number.EPSILON) * 100) / 100;
-        console.log(segons + " segons");
-        console.log(min + " minuts");
-        console.log(h + " hores")
-        console.log(startTime);
-        console.log(endTime);
+    // get seconds 
+    var segons = Math.round(timeDiff);
+    var minuts = segons/60;
+    var min = Math.round((minuts + Number.EPSILON) * 100) / 100;
+    var hores = minuts/60;
+    h = Math.round((hores + Number.EPSILON) * 100) / 100;
+    console.log(segons + " segons");
+    console.log(min + " minuts");
+    console.log(h + " hores")
+    console.log(startTime);
+    console.log(endTime);
 
-        return endTime;
-        }
+    return endTime;
+    }
 
-        function enviant(){
-        $('#send').click( function() {
-            alert('funco send');
-            console.log('funco send');
-            $.ajax(
-                    {
-                        type: "POST",
-                        url: "{{route('activitat-form.store')}}",
-                        data:$('#form-temps').serialize(),
-                        success: function( data ) {
-                            console.log(data);
-                            $("#input-treballador").val();
-                            $("#total_cron").val(h);
-                            /* $("#inici-jornada").val(startTime);
-                            $("#final-jornada").val(endTime); */
-                            
-                        }
+    function enviant(){
+    $('#send').click( function() {
+        alert('funco send');
+        console.log('funco send');
+        $.ajax(
+                {
+                    type: "POST",
+                    url: "{{route('home.store')}}",
+                    data:$('#form-temps').serialize(),
+                    success: function( data ) {
+                        console.log(data);
+                        $("#input-treballador").val();
+                        $("#total_cron").val(h);
+                        /* $("#inici-jornada").val(startTime);
+                        $("#final-jornada").val(endTime); */
+                        
                     }
-                )
-            }
-            );
+                }
+            )
         }
-
-
-
-        
+        );
+    }
 
        /*  $('#form-temps').submit(function(e)){
             e.preventDefault();
@@ -107,10 +88,7 @@
             });
         }); */
 
-       
-
-        </script>
-</head>
+</script>
 
 
 
@@ -127,26 +105,22 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <form id="form-temps" action="{{route('home.store')}}" method="post">
 
-                    Benvingut {{$user->name}}!
-                    <br>
-                    id: {{$user->id}}
+                        @csrf        
+                        
+                        <input type="text" name="treballador" id="treballador" value="{{$user->id}}">
+            
+                        <button type="button" class="btn btn-dark btn-block" onclick="start()">Començar a comptar</button>
+            
+                        <button type="button" class="btn btn-light btn-block" onclick="end()">Parar de comptar</button>
+            
+                        <input type="hidden" id="total_cron">
+                        <input type="hidden" id="inici-jornada">
+                        <input type="hidden" id="final-jornada">
+                        <button type="submit" id="send" value="Enviar" class="btn btn-dark btn-block">Enviar</button>   
+                    </form>
                 </div>
-                <form id="form-temps" action="{{route('activitat-form.store')}}" method="post">
-
-                    @csrf        
-                    
-                    <input type="text" name="treballador" id="treballador">
-        
-                    <button type="button" class="btn btn-dark btn-block" onclick="start()">Començar a comptar</button>
-        
-                    <button type="button" class="btn btn-light btn-block" onclick="end()">Parar de comptar</button>
-        
-                    <input type="hidden" id="total_cron">
-                    <input type="hidden" id="inici-jornada">
-                    <input type="hidden" id="final-jornada">
-                    <button type="submit" id="send" value="Enviar" class="btn btn-dark btn-block">Enviar</button>   
-                </form>
 
             </div>
         </div>
